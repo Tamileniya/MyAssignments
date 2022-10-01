08e55316e2092b2d4784a755145a7fc2394e3475
Tamileniya <114818300+Tamileniya@users.noreply.github.com>
2022-10-01 19:59:14 +0530

    Add files via upload
    day1 Assignment

---
+++ day1/car.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class car {
+	int km = 500;
+	byte speed = 55;
+	short year = 2022;
+	long chaseno = 123456789;
+	float customerno = 345F;
+	char carName = 'M';
+	double reg = 123456.98456;
+	boolean doorOpen = true;
+	String brand = "BMW";
+}
